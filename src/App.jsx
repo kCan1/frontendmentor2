710a3c17487781a0b04978fbf5d830c55e27004a
kCan1 <email@youve-registered-with-kCan1>
2025-06-30 22:33:49 +0800

    updated with mark siaozn guide

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,13 @@ import BlogCard from "./components/BlogCard";
 function App() {
   return (
     <>
-      <BlogCard />
+      <main className="mainHomeScreen">
+        <section>
+          <div className="w-full max-w-[300px] max-h-[500] drop-shadow-[6px_6px_0px_rgba(0,0,0,1)]">
+            <BlogCard />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -4,19 +4,25 @@ import topImage from "../assets/images/illustration-article.svg";
 
 const BlogCard = () => {
   return (
-    <div className="card md:min-h-screen sm:min-h-screen sm:m-12 border-2 border-b-16 border-r-16 rounded-xl">
+    <div className="card border-1 rounded-xl ">
       <img className="img-card" src={topImage} alt="Avatar" />
-      <p>
-        <h1>Learning</h1>
-        <h2>Published 21 Dec 2023</h2>
-        <h3>HTML & CSS Foundation</h3>
-        <p>
-          These languages are the backbone of every website, defining structure,
-          content, and presentation.
-        </p>
-        <div className="flex flex-row">
-          <img className="" src={avatar} alt="Avatar" />
-          <h1>Greg Hooper</h1>
+      <section className="flex flex-row items-start justify-start">
+        <div className="bg-yellow-400 rounded-sm p-1 mt-2">
+          <h3 className="text-xs font-bold px-1">Learning</h3>
+        </div>
+      </section>
+      <h2 className="text-xs font-base">Published 21 Dec 2023</h2>
+      <h1 className="font-extrabold text-lg hover:text-yellow-400">
+        HTML & CSS Foundation
+      </h1>
+      <p className="text-xs font-light tracking-wide">
+        These languages are the backbone of every website, defining structure,
+        content, and presentation.
+        <div className="flex flex-row gap-3 mt-4">
+          <img className="w-6" src={avatar} alt="Avatar" />
+          <h1 className="text-xs flex flex-row items-center font-bold">
+            Greg Hooper
+          </h1>
         </div>
       </p>
     </div>
